b7b81084e35541110b0988541532567d06c46c0f
EduOrtegaG <131004238+EduardoOrtega2003@users.noreply.github.com>
2025-03-28 22:44:29 +0000

    Add files via upload

---
+++ Ortega_Ramírez_Eduardo_ActEv_UT5_Final/src/Password.java
@@ -0,0 +1,24 @@
+import java.security.*;
+import java.util.Base64;
+
+public class Password {
+
+    public static String hashPassword(String password) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            byte[] hashedBytes = md.digest(password.getBytes());
+            return Base64.getEncoder().encodeToString(hashedBytes);
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+    
+    public static boolean verifyPassword(String password, String storedHash) {
+        return hashPassword(password).equals(storedHash);
+    }
+    
+    public static boolean isValidPassword(String password) {
+        return password.length() >= 6;
+    }
+}
